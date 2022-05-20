140b5bf4a7e2211e1f293cad2768de6af1c304b0
inewh <inewh@DESKTOP-HHQPVN0.attlocal.net>
2022-05-19 20:44:59 -0500

    Comit

---
+++ CSIA/src/database/Data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE players (id INTEGER PRIMARY KEY AUTOINCREMENT, num INTEGER, name TEXT, position TEXT, age INTEGER);
+
+INSERT INTO players VALUES (1,"Isaac Meriwn", "Goalie", 19);
+
+SELECT * FROM players;
